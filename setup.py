631cfaff69a260203b71fa82017c070c424ef3a1
Arturo Filastò <art@fuffa.org>
2014-08-09 18:16:22 +0200

    Make the setup.py script link to the currently installed libdnet

--- setup.py
+++ setup.py
@@ -3,52 +3,92 @@
 import sys
 
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 
-dnet_srcs = ['dnet/dnet.c']
-dnet_incdirs = []
-dnet_libdirs = []
-dnet_libs = []
-dnet_extargs = []
-dnet_extobj = []
+src = ['dnet/dnet.c']
+dep = []
+Libraries = []
+IncludeDirs = []
+LibraryDirs = []
 
-if sys.platform == 'win32':
-    winpcap_dir = '../../WPdpack'
-    dnet_srcs.extend(['../src/addr-util.c',
-                      '../src/addr.c',
-                      '../src/blob.c',
-                      '../src/ip-util.c',
-                      '../src/ip6.c',
-                      '../src/rand.c',
-                      '../src/err.c',
-                      '../src/strlcat.c',
-                      '../src/strlcpy.c',
-                      '../src/err.c',
-                      '../src/strlcat.c',
-                      '../src/strlcpy.c',
-                      '../src/strsep.c',
-                      '../src/arp-win32.c',
-                      '../src/eth-win32.c',
-                      '../src/fw-pktfilter.c',
-                      '../src/intf-win32.c',
-                      '../src/ip-win32.c',
-                      '../src/route-win32.c',
-                      '../src/tun-none.c'])
-    dnet_incdirs.append(winpcap_dir + '/Include')
-    dnet_libdirs.append(winpcap_dir + '/Lib')
-    dnet_libs.extend(['advapi32', 'iphlpapi', 'ws2_32', 'packet'])
+if sys.platform == 'win32' or sys.platform == "nt":
+    Libraries = ["ws2_32", "advapi32", "iphlpapi", "ws2_32", "packet"]
 
-dnet = Extension('dnet',
-                 dnet_srcs,
-                 include_dirs=dnet_incdirs,
-                 library_dirs=dnet_libdirs,
-                 libraries=dnet_libs)
+    class BuildExtension(build_ext):
+        user_options = (build_ext.user_options +
+                        [("with-dnet=", None,
+                          "directory where dnet is installed"),
+                         ("with-winpcap=", None,
+                          "directory where winpcap is installed")])
+        with_dnet = None
+        with_winpcap = None
+
+        dnet_dlls = ()
+        dnet_mingw = False
+
+        def finalize_options(self):
+            build_ext.finalize_options(self)
+            if self.with_dnet is None:
+                self.find_dnet()
+            if self.with_winpcap is None:
+                self.find_winpcap()
+            self.find_dnet_dlls()
+            self.add_dnet_compile_info()
+
+        def find_dnet(self):
+            """
+            Find dnet's install directory.
+
+            TODO get windows machine to implement this.
+            """
+            pass
+
+        def find_dnet_dlls(self):
+            self.dnet_dlls = []
+            self.find_dnet_dll()
+
+        def find_dnet_dll(self):
+            pass
+
+        def find_winpcap_dir(self):
+            self.winpcap_dir = "/"
+
+        def add_dnet_compile_info(self):
+            self.include_dirs.append(self.with_winpcap + '/Include')
+            self.library_dirs.append(self.with_winpcap + '/Lib')
+            # self.include_dirs.append()
+            # self.library_dirs.append()
+            # self.libraries.extend(["libdnet"])
+
+        def run(self):
+            build_ext.run(self)
+            for dllpath, newpath in self.dnet_dlls:
+                self.copy_file(dllpath, newpath)
+
+        def get_outputs(self):
+            output = [pathpair[1] for pathpair in self.dnet_dlls]
+            output.extend(build_ext.get_outputs(self))
+            return output
+else:
+    BuildExtension = build_ext
+    Libraries = ["dnet"]
+
+dnet_extension = Extension('dnet',
+                           src,
+                           libraries=Libraries,
+                           depends=dep,
+                           include_dirs=IncludeDirs,
+                           library_dirs=LibraryDirs)
 
 setup(
     name='dnet',
     version='1.12',
+    packages=["dnet"],
+    package_dir={"dnet": "dnet"},
     description='low-level networking library',
     author='Dug Song',
     author_email='dugsong@monkey.org',
+    cmdclass={"build_ext": BuildExtension},
     url='http://libdnet.sourceforge.net/',
-    ext_modules=[dnet]
+    ext_modules=[dnet_extension]
 )
